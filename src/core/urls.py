06eec71712b4586f1bc4f8b56c26ee0187000c6e
Bruno Pascoal <bpascoal.santos@gmail.com>
2025-04-30 15:21:57 -0300

    style: fix logo login page

--- src/core/urls.py
+++ src/core/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("api/v1/", include("authentication.urls")),
     path("api/v1/", include("customers.urls")),
     path("api/v1/", include("parking.urls")),
     path("api/v1/", include("vehicles.urls")),
+    path("", admin.site.urls),
+
 ]
