6c38d45c8ce2ebfa3831421bea26fdac99b05f2c
Bruno Pascoal <bpascoal.santos@gmail.com>
2025-05-01 10:44:56 -0300

    docs: added redoc

--- src/core/urls.py
+++ src/core/urls.py
@@ -3,12 +3,14 @@ from django.urls import include, path
 
 from drf_spectacular.views import (
   SpectacularAPIView, 
-  SpectacularSwaggerView
+  SpectacularSwaggerView,
+  SpectacularRedocView
 )
 
 urlpatterns = [
     path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/v1/docs/", SpectacularSwaggerView.as_view(url_name='schema'), name="swagger-ui"),
+    path("api/v1/redoc/", SpectacularRedocView.as_view(url_name='schema'), name="redoc"),
     path("api/v1/", include("authentication.urls")),
     path("api/v1/", include("customers.urls")),
     path("api/v1/", include("parking.urls")),
